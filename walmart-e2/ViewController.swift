b1d45802f1dd9617316196727e4766dfb76d1d9c
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2024-10-08 21:12:43 -0600

    Base code template

---
+++ walmart-e2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walmart-e2
+//
+//  Created by Personal on 08/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ walmart-e2UITests/walmart_e2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  walmart_e2UITestsLaunchTests.swift
+//  walmart-e2UITests
+//
+//  Created by Personal on 08/10/24.
+//
+
+import XCTest
+
+final class walmart_e2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
